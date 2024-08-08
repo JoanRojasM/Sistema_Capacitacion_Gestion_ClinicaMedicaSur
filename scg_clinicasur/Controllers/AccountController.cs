c50be5cf341dd8166e30346ec04d5d1c36c06854
Samuel Alvarado <Samuelalvaradov_@outlook.com>
2024-08-07 22:10:40 -0600

    Login
    login y recuperación de contraseña con alertas

---
+++ scg_clinicasur/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace scg_clinicasur.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (username == "Samuel" && password == "123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Usuario o Contraseña Incorrectos.";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ForgotPassword(string email)
+        {
+            if (email == "samuel@outlook.com")
+            {
+                ViewBag.SuccessMessage = "Enviado con éxito.";
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Correo Electrónico Incorrecto.";
+            }
+            return View();
+        }
+    }
+}
