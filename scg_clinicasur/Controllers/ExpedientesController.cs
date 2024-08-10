850ba26ddfbdb67d6141a4bd51809ffc80292b33
ZeroDaniel27 <danielaharoni27@gmail.com>
2024-08-10 16:51:39 -0600

    Creación del apartado Expedientes
    Se realizó la implementación de la HU que solicitaba un apartado de expedientes.

---
+++ scg_clinicasur/Controllers/ExpedientesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace scg_clinicasur.Controllers
+{
+    public class ExpedientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Historial()
+        {
+            return View();
+        }
+
+        public IActionResult BusquedaExpedientes()
+        {
+            return View();
+        }
+
+        public IActionResult DetallesConsulta(int id)
+        {
+            return View();
+        }
+    }
+}
