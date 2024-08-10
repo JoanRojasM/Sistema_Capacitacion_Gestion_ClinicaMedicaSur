02a17d9a5ab82dcd792b2b71aec4dd31d205a704
ZeroDaniel27 <danielaharoni27@gmail.com>
2024-08-10 15:51:59 -0600

    Creación del apartado Notificaciones
    Se realizó la implementación de la HU que solicitaba un apartado de notificaciones para los usuarios. También se realizaron cambios en la estructura y estilo de la página.

---
+++ scg_clinicasur/Controllers/NotificacionesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace scg_clinicasur.Controllers
+{
+    public class NotificacionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detalles(int id)
+        {
+            ViewData["NotificationId"] = id;
+            ViewData["Tipo"] = "Recordatorio";
+            ViewData["Mensaje"] = "Consulta programada para mañana.";
+            ViewData["Fecha"] = "2024-08-10";
+
+            return View();
+        }
+
+        public IActionResult Configuracion()
+        {
+            return View();
+        }
+    }
+}
