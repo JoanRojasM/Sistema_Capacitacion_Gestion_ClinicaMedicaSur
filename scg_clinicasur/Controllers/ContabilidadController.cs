f4b3c6504c38df281abd2e84d9a31fe0257f0864
ZeroDaniel27 <danielaharoni27@gmail.com>
2024-08-10 16:35:51 -0600

    Creación del apartado Contabilidad
    Se realizó la implementación de la HU que solicitaba un apartado de contabilidad.

---
+++ scg_clinicasur/Controllers/ContabilidadController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace scg_clinicasur.Controllers
+{
+    public class ContabilidadController : Controller
+    {
+        public IActionResult AgregarEntrada()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrarSalida()
+        {
+            return View();
+        }
+
+        public IActionResult ReporteMensual()
+        {
+            return View();
+        }
+
+        public IActionResult Historial()
+        {
+            return View();
+        }
+    }
+
+}
