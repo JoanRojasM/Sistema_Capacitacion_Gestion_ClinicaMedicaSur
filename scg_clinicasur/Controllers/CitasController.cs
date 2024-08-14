b86031e545c54e4956d1c961b1919d7f514a2965
ZeroDaniel27 <danielaharoni27@gmail.com>
2024-08-14 09:36:40 -0600

    Corrección de Errores
    Se corrigieron algunos errores con el ingreso al apartado de citas.

---
+++ scg_clinicasur/Controllers/CitasController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace scg_clinicasur.Controllers
+{
+    public class CitasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Calendario()
+        {
+            return View();
+        }
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Eliminar()
+        {
+            return View();
+        }
+    }
+}
