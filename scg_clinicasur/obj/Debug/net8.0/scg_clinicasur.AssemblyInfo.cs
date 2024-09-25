b11f71cf312348f08b6b88b54e09727c91bbd18e
JoanRojasM <jrojas30463@ufide.ac.cr>
2024-09-24 20:17:46 -0600

    Se añade el script de la base de datos
    Se añade el script de la base de datos

--- scg_clinicasur/obj/Debug/net8.0/scg_clinicasur.AssemblyInfo.cs
+++ scg_clinicasur/obj/Debug/net8.0/scg_clinicasur.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scg_clinicasur")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc5e7d1f3c89dbaa5db3b67c427e84a5ce55a0f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46a8c3ecd3cac17b76678c35f72fa7851a98af3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("scg_clinicasur")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scg_clinicasur")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
