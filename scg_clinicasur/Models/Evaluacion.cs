adfcf96885236bd4dd4728d10e1b58206c39aecb
JoanRojasM <jrojas30463@ufide.ac.cr>
2024-10-29 18:56:02 -0600

    Mejorando Capacitación
    Se han solucionado los problemas presentes en capacitación

--- scg_clinicasur/Models/Evaluacion.cs
+++ scg_clinicasur/Models/Evaluacion.cs
@@ -4,29 +4,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace scg_clinicasur.Models
 {
+    [Table("evaluacion")] // Especificar el nombre de la tabla
     public class Evaluacion
     {
         [Key]
         public int id_evaluacion { get; set; }
 
-       
-        [StringLength(255)]
+        [StringLength(100)] // Coincide con VARCHAR(100) en la tabla
         public string nombre { get; set; }
 
-        [StringLength(1000)]
+        [Column(TypeName = "TEXT")] // Definir como tipo TEXT
         public string descripcion { get; set; }
 
-        public TimeSpan tiempo_prueba { get; set; }
+        // Permitir null para 'tiempo_prueba' si es necesario
+        public TimeSpan? tiempo_prueba { get; set; }
 
         [StringLength(255)]
         public string archivo { get; set; }
 
         [ForeignKey("Usuario")]
-        public int id_usuario { get; set; }
-        public Usuario Usuario { get; set; }
+        public int? id_usuario { get; set; } // Permitir null para la clave foránea
+        public Usuario? Usuario { get; set; }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public DateTime fecha_creacion { get; set; } // Nueva propiedad
+        public DateTime fecha_creacion { get; set; } // Fecha de creación automática
     }
 }
-
