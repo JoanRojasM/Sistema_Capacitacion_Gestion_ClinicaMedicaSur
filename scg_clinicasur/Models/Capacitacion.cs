928419ba84da7d5538cf92bb0a89000734a67e00
ZeroDaniel27 <danielaharoni27@gmail.com>
2024-11-07 14:34:01 -0600

    Modificaciones en Vistas y el Modelo de Capacitación
    Se realizaron pequeñas modificaciones en la vista crear y el modelo de capacitación para mejorar su funcionalidad

--- scg_clinicasur/Models/Capacitacion.cs
+++ scg_clinicasur/Models/Capacitacion.cs
@@ -14,13 +14,14 @@ namespace scg_clinicasur.Models
         [StringLength(255)]
         public string titulo { get; set; }
 
+        [Required(ErrorMessage = "La descripción es obligatoria")]
         [StringLength(1000)]
         public string descripcion { get; set; }
 
+        [Required(ErrorMessage = "La duración es obligatoria")]
         [StringLength(255)]
         public string duracion { get; set; }
 
-        // id_usuario no requerido para permitir NULL en la base de datos
         [ForeignKey("Usuario")]
         public int? id_usuario { get; set; }
         public Usuario? Usuario { get; set; }
@@ -34,7 +35,7 @@ namespace scg_clinicasur.Models
         [StringLength(500)]
         public string? enlace { get; set; }
 
-        // Estado permitido como NULL y sin inicialización por defecto
+        [Required(ErrorMessage = "El estado es obligatorio")]
         [StringLength(10)]
         public string estado { get; set; }
     }
