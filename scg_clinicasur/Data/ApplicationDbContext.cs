5f2a35d7b92577f5a00cb79f78747b5ae945295a
JoanRojasM <jrojas30463@ufide.ac.cr>
2024-11-12 19:46:51 -0600

    Reapply "Calendario"
    This reverts commit 5410bb398d2caac3b85671203690098def62c0e8.

--- scg_clinicasur/Data/ApplicationDbContext.cs
+++ scg_clinicasur/Data/ApplicationDbContext.cs
@@ -18,5 +18,6 @@ namespace scg_clinicasur.Data
         public DbSet<scg_clinicasur.Models.Capacitacion> Capacitaciones { get; set; }
         public DbSet<scg_clinicasur.Models.EstadoCita> EstadoCitas { get; set; }
         public DbSet<scg_clinicasur.Models.Cita> Citas { get; set; }
+        public DbSet<scg_clinicasur.Models.DisponibilidadDoctor> DisponibilidadDoctor { get; set; } 
     }
 }
