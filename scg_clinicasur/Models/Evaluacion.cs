d17e220199e94bdf3e764ae2a72da5d3dded17e7
Samuel Alvarado <Samuelalvaradov_@outlook.com>
2024-12-16 17:57:01 -0600

    Evaluaciones crear ,detalles y eliminar

--- scg_clinicasur/Models/Evaluacion.cs
+++ scg_clinicasur/Models/Evaluacion.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
 
 namespace scg_clinicasur.Models
 {
@@ -20,16 +21,12 @@ namespace scg_clinicasur.Models
         public string nombre { get; set; }
 
         [Required(ErrorMessage = "La descripción es obligatoria")]
-        [Column(TypeName = "TEXT")]
+        [StringLength(100)]
         public string descripcion { get; set; }
 
         [Required(ErrorMessage = "El tiempo de prueba es obligatorio")]
         [StringLength(255)]
         public string tiempo_prueba { get; set; }
-
-        [Required(ErrorMessage = "El archivo es obligatorio")]
-        [StringLength(255)]
-        public string archivo { get; set; }
 
         [ForeignKey("Usuario")]
         public int? id_usuario { get; set; }
