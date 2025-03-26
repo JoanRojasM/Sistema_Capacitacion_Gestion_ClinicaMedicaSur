82ab820acc36ecd1453b8f5f4623dc431e4c177d
JoanRojasM <133838320+JoanRojasM@users.noreply.github.com>
2025-03-25 20:51:37 -0600

    Hosting

--- scg_clinicasur/obj/Debug/net8.0/scg_clinicasur.AssemblyInfo.cs
+++ scg_clinicasur/obj/Debug/net8.0/scg_clinicasur.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scg_clinicasur")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff7dbe8594ef12cc6d72e120147bba74650168f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3421ad30fcc455c1950160c3d22bced8d11952")]
 [assembly: System.Reflection.AssemblyProductAttribute("scg_clinicasur")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scg_clinicasur")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
