256aae698304877d6a82c686eba190bb3a963e63
JoanRojasM <133838320+JoanRojasM@users.noreply.github.com>
2025-03-25 21:07:34 -0600

    hosting #2

--- scg_clinicasur/obj/Debug/net8.0/scg_clinicasur.AssemblyInfo.cs
+++ scg_clinicasur/obj/Debug/net8.0/scg_clinicasur.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scg_clinicasur")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3421ad30fcc455c1950160c3d22bced8d11952")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54f1bf122f0124d01fe63124aa73ce3ec919c43")]
 [assembly: System.Reflection.AssemblyProductAttribute("scg_clinicasur")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scg_clinicasur")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
